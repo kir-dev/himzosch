cf3efa187ff172f5617343a40f8920485f0a10b7
Bucsy Benjámin <bucsy.benjamin@simonyi.bme.hu>
2022-10-18 00:03:52 +0200

    add prod config

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root 'site#home'
+
+  # redirect to route on not found error
+  # temporary solution for staging
+  # TODO: remove this after demo is done
+  match '*unmatched', to: redirect(''), via: :get
 end
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,19 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /himzosch/tmp/pids/server.pid
 
-# If the database exists, migrate. Otherwise setup (create and migrate)
-echo "Running database migrations..."
-bundle exec rails db:prepare
-echo "Finished running database migrations."
+
+if [ "$RAILS_ENV" == "production" ]
+then
+
+  # If the database exists, migrate. Otherwise setup (create and migrate)
+  echo "Running database migrations..."
+  bundle exec rails db:prepare
+  echo "Finished running database migrations."
+
+  # Clean assets & precompile newest assets
+  echo "Compiling assets..."
+  bundle exec rails assets:clobber
+  bundle exec rails assets:precompile
+fi
 
 exec "$@"

0b08a4005a24ae600c1ef95b762e4bf0f31be23f
Andai Roland <r.andai@outlook.com>
2023-02-06 15:44:27 +0100

    embedded google forms

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_16_093703) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_06_134353) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_16_093703) do
     t.string "title"
     t.text "intro"
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

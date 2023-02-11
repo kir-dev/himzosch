9a90f36167ba92dd1f62a73dc3b033e713ed59f5
Benjamin Bucsy <benjamin.bucsy@benestudio.co>
2023-02-11 17:27:58 +0100

    Finish gallery policies

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,6 +1,6 @@
 class PicturesController < ApplicationController
   before_action :set_picture, only: %i[ show edit update destroy ]
-
+  before_action :require_admin, except: %i[index show]
   # GET /pictures or /pictures.json
   def index
     @pictures = Picture.all

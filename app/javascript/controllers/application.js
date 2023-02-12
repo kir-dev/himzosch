b7c06992ca9a14ffb1bf9e3e4705b91066687f70
Bucsy Benjámin <bucsy.benjamin@simonyi.bme.hu>
2023-02-12 11:33:47 +0100

    fix importmaps in production

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,6 +1,5 @@
 import { Application } from "@hotwired/stimulus"
 import Notification from 'stimulus-notification'
-import Gallery from './gallery_controller'
 
 const application = Application.start()
 application.register('notification', Notification)

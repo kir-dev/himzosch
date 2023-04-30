8e802f23ba10c69a4980baa5b290e1ff5a1a77fb
Andai Roland <r.andai@outlook.com>
2023-04-30 12:23:56 +0200

    index layout changed to grid

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,7 +1,7 @@
 class Member < ApplicationRecord
   has_one_attached :image
 
-  validates :name, presence: true, length: { maximum: 50 }
-  validates :rank, presence: true, length: { maximum: 50 }
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :rank, presence: true, length: { maximum: 15 }
   validates :bio, length: { maximum: 200 }
 end
